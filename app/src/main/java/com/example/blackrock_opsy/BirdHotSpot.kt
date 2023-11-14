22f9df8933ba8a9dabb4fb8f2a219e016099cf85
codenameKidsNextD00r <mlumbibongani@gmail.com>
2023-11-14 14:50:13 +0200

    Updated map and added API key to local properties

--- app/src/main/java/com/example/blackrock_opsy/BirdHotSpot.kt
+++ app/src/main/java/com/example/blackrock_opsy/BirdHotSpot.kt
@@ -9,49 +9,36 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
-import com.example.blackrock_opsy.databinding.ActivityBirdHotSpotBinding
+import com.example.blackrock_opsy.databinding.ActivityMapBinding
 
-class BirdHotSpot : AppCompatActivity(), OnMapReadyCallback {
+class MapsMarkerActivity : AppCompatActivity(), OnMapReadyCallback {
 
-    private lateinit var mMap: GoogleMap
-    private lateinit var binding: ActivityBirdHotSpotBinding
-
+    // [START_EXCLUDE]
+    // [START maps_marker_get_map_async]
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // Retrieve the content view that renders the map.
+        setContentView(R.layout.activity_map)
 
-        binding = ActivityBirdHotSpotBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        val mapFragment = supportFragmentManager
-            .findFragmentById(R.id.map) as SupportMapFragment
-        mapFragment.getMapAsync(this)
+        // Get the SupportMapFragment and request notification when the map is ready to be used.
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as? SupportMapFragment
+        mapFragment?.getMapAsync(this)
     }
+    // [END maps_marker_get_map_async]
+    // [END_EXCLUDE]
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+    // [START maps_marker_on_map_ready_add_marker]
     override fun onMapReady(googleMap: GoogleMap) {
-        mMap = googleMap
-
-        // Add a marker in Sydney and move the camera
-
-        val sydney = LatLng(-26.3000061, 28.2076127)
-        mMap.addMarker(MarkerOptions().position(sydney).title( "Rondebult Bird Sanctuary"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
-
-       /* val capeTown = LatLng(-33.9249, 18.4241)
-        mMap.addMarker(MarkerOptions().position(capeTown).title("Marker in Cape Town, South Africa"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(capeTown))
-
-        val Durban = LatLng(-29.857606096558232, 31.011207990558702)
-        mMap.addMarker(MarkerOptions().position(Durban).title("Marker in Durban, South Africa"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(Durban))*/
+        val sydney = LatLng(-26.30041843095174, 28.20732836593826)
+        googleMap.addMarker(
+            MarkerOptions()
+                .position(sydney)
+                .title("Rondebult Bird Sanctuary")
+        )
+        // [START_EXCLUDE silent]
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        // [END_EXCLUDE]
     }
+    // [END maps_marker_on_map_ready_add_marker]
 }
+// [END maps_marker_on_map_ready]}
